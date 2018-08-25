7c172e999173e2c0a8c17fd23f202adffa3f43c8
Elton John <37675998+EltonBR@users.noreply.github.com>
2018-08-25 17:51:26 -0300

    Update and rename main.c to example.c

--- example.c
+++ example.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-
+    
+    //initialize pins
     shiftOut_begin();
 
     while (1) {
-            _delay_ms(100);
+        
+        _delay_ms(100);
+        //send 0xF0 to shift register Q0..Q7 -> 00001111
         shiftOut_send(0xF0);
     }
 
